b0ee4fffe8879d2b721759bde15483cec8fbefb1
epixian <epixian@epixian.com>
2019-05-10 18:42:10 -0400

    store visit in database

--- app/Http/Controllers/LandingsController.php
+++ app/Http/Controllers/LandingsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Visit;
 
 class LandingsController extends Controller
 {
@@ -11,6 +12,14 @@ class LandingsController extends Controller
         $userdata = json_decode(base64_decode(request('utm_term')));
         
         if ($userdata !== null) {
+
+            Visit::create([
+                'utm_source' => request('utm_source'),
+                'utm_medium' => request('utm_medium'),
+                'utm_campaign' => request('utm_campaign'),
+                'utm_term' => request('utm_term'),
+                'utm_content' => request('utm_content')
+            ]);
             return view('landing', compact('userdata'));
         }
         else {
