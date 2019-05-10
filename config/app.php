6c092ea6cf6472c8486e0fc2ae97931b28c92eac
epixian <epixian@epixian.com>
2019-05-10 11:44:59 -0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'UTM Landing'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://utm-landing.test:8002'),
 
     'asset_url' => env('ASSET_URL', null),
 
