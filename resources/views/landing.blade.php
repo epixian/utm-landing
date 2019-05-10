1e7b42bbe50a88308b7ae71c36b2d5126bc4585c
epixian <epixian@epixian.com>
2019-05-10 09:47:32 -0400

    initial commit

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{ config('app.name', 'Snowman') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="root">
+        
+    </div>
+</body>
+</html>
