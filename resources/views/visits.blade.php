e1dbd9175ad0fe962268d6db0581751033fa0944
EPIXIAN LLC <epixian@epixian.com>
2019-05-16 11:38:24 -0400

    redesign visits page

--- resources/views/visits.blade.php
+++ resources/views/visits.blade.php
@@ -36,36 +36,6 @@
             </div>
             @endforeach
         </div>
-        <table class="text-sm">
-            <thead>
-                <tr>
-                    <td>ID</td>
-                    <td>Source</td>
-                    <td>Medium</td>
-                    <td>Campaign</td>
-                    <td>Term</td>
-                    <td>Content</td>
-                    <td>First</td>
-                    <td>Last</td>
-                    <td>Approved</td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($visits as $visit)
-                <tr>
-                    <td>{{ $visit->id }}</td>
-                    <td>{{ $visit->utm_source }}</td>
-                    <td>{{ $visit->utm_medium }}</td>
-                    <td>{{ $visit->utm_campaign }}</td>
-                    <td>{{ $visit->utm_term }}</td>
-                    <td>{{ $visit->utm_content }}</td>
-                    <td>{{ json_decode(base64_decode($visit->utm_term))->first_name }}</td>
-                    <td>{{ json_decode(base64_decode($visit->utm_term))->last_name }}</td>
-                    <td>{{ json_decode(base64_decode($visit->utm_term))->approved }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
     </div>
 </body>
 </html>
